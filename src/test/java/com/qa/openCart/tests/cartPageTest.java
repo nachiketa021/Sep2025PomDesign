cfeb9d8adbf2013de3437cf76892c7f896bfdd85
nachiketa021 <nachiketa021@gmail.com>
2025-09-08 19:58:15 +0530

    added cartTest

---
+++ src/test/java/com/qa/openCart/tests/cartPageTest.java
@@ -0,0 +1,11 @@
+package com.qa.openCart.tests;
+
+import org.testng.annotations.Test;
+
+public class cartPageTest {
+
+	@Test
+	public void cartTest() {
+		System.out.println("Cart Testing.");
+	}
+}
